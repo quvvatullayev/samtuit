e82e9962803a2ee8dc414dd2101bb220f766e6dd
ogabek <quvvatullayev@gmail.com>
2023-04-28 15:47:54 +0500

    fix db and bot

--- bot.py
+++ bot.py
@@ -8,8 +8,21 @@ db = DB("db.json")
 class Samtuit:
     def start(self,update:Update,context:CallbackContext):
         bot = context.bot
-        chat_id = update.message.chat_id
-        db.add_job(chat_id, "start")
+        chat_id = int(update.message.chat_id)
+        job = db.get_job(chat_id)
+        admin = db.get_admin(chat_id)
+        
+        if admin:
+            print(admin)
+            text = "You are admin"
+            bot.send_message(chat_id=chat_id, text=text)
 
-        reply_markup = ReplyKeyboardMarkup([["/start"]], resize_keyboard=True)
-        bot.send_message(chat_id=chat_id, text="Hello", reply_markup=reply_markup)
+        elif job:
+            print(job)
+            text = "You have a job"
+            bot.send_message(chat_id=chat_id, text=text)
+
+        else:
+            print(chat_id)
+            text = "user"
+            bot.send_message(chat_id=chat_id, text=text)
--- db.py
+++ db.py
@@ -12,32 +12,32 @@ class DB:
     """
     def __init__(self, path) -> None:
         self.db = TinyDB(path, indent=4, separators=(',', ': '))
-        self.table = self.db.table("jobs")
-        self.table = self.db.table("admin")
+        self.jobs = self.db.table("jobs")
+        self.admin = self.db.table("admin")
         self.query = Query()
         
     def add_job(self, chat_id, job_name):
-        if self.table.get(doc_id=chat_id):
+        if self.jobs.get(doc_id=chat_id):
             return False
         else:
-            self.table.insert(Document({"chat_id": chat_id, "job_name": job_name}, doc_id=chat_id))
+            self.jobs.insert(Document({"chat_id": chat_id, "job_name": job_name}, doc_id=chat_id))
             return True
 
     def add_admin(self, chat_id):
-        if self.table.get(doc_id=chat_id):
+        if self.admin.get(doc_id=chat_id):
             return False
         else:
-            self.table.insert(Document({"chat_id": chat_id}, doc_id=chat_id))
+            self.admin.insert(Document({"chat_id": chat_id}, doc_id=chat_id))
             return True
         
     def get_job(self, chat_id):
-        return self.table.get(doc_id=chat_id)
+        return self.jobs.get(doc_id=chat_id)
     
     def get_admin(self, chat_id):
-        return self.table.get(doc_id=chat_id)
+        return self.admin.get(doc_id=chat_id)
     
     def get_all_jobs(self):
-        return self.table.all()
+        return self.jobs.all()
     
     def get_all_admins(self):
-        return self.table.all()
+        return self.admin.all()
